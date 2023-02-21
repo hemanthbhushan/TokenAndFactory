1bead5770ffc89e55bb8993b7e79ba28254659c4
Hemanth Bhushan <hemanth.bhushan@antiersolutions.com>
2023-02-21 16:38:58 +0530

    commit message

--- contracts/TokenContract.sol
+++ contracts/TokenContract.sol
@@ -12,7 +12,7 @@ contract TokenContract is ERC20,Ownable{
 
    
     mapping(address => bool) internal frozen;
-    mapping(address => bool) internal blankList;
+    mapping(address => bool) internal _blacklisted;
     mapping(address => uint256) internal frozenTokens;
 
     
@@ -21,7 +21,7 @@ contract TokenContract is ERC20,Ownable{
 
     event AddressFrozen(address indexed _userAddress, bool indexed _isFrozen, address indexed _owner);
     event AddressUnfrozen(address indexed addr);
-    event blankListed(address[] indexed addr);
+    event blackListed(address[] indexed addr);
     event removeBlankListed(address[] indexed addr);
     event TokensFrozen(address indexed _userAddress, uint256 _amount);
     event TokensUnfrozen(address indexed _userAddress, uint256 _amount);
@@ -31,12 +31,20 @@ contract TokenContract is ERC20,Ownable{
         require(adminAddress == msg.sender, "onlyAdmin");
         _;
          }
+
+    modifier notBlacklisted(address account) {
+        require(
+            !_blacklisted[account],
+            "Blacklistable: account is blacklisted"
+        );
+        _;
+    }     
     
     
 
     constructor(string memory _name,string memory _symbol)ERC20(_name,_symbol){}
 
-    function mint(address _to, uint256 _amount) public  onlyAdmin {
+    function mint(address _to, uint256 _amount) public notBlacklisted(msg.sender) notBlacklisted(_to) onlyAdmin {
       
         _mint(_to, _amount);
       
@@ -44,7 +52,7 @@ contract TokenContract is ERC20,Ownable{
 
 
 
-    function burn(address _userAddress, uint256 _amount) external  onlyAdmin {
+    function burn(address _userAddress, uint256 _amount) external notBlacklisted(msg.sender) notBlacklisted(_userAddress) onlyAdmin {
       uint256 freeBalance = balanceOf(_userAddress) - frozenTokens[_userAddress];
         if (_amount > freeBalance) {
             uint256 tokensToUnfreeze = _amount - (freeBalance);
@@ -55,15 +63,25 @@ contract TokenContract is ERC20,Ownable{
       
     }
 
+    function transfer(address _to,uint256 _amount)public override notBlacklisted(msg.sender) notBlacklisted(_to) returns(bool) {
+        require(_amount <= balanceOf(msg.sender) - (frozenTokens[msg.sender]), "Insufficient Balance");
+        transfer(_to,_amount);
+    }
 
-    function freezeTokens(address _userAddress, uint256 _amount) external  onlyAdmin {
+    function transferFrom(address _from,address _to,uint256 _amount)public override notBlacklisted(msg.sender) notBlacklisted(_from) notBlacklisted(_to) returns(bool) {
+        require(_amount <= balanceOf(_from) - (frozenTokens[_from]), "Insufficient Balance");
+        transferFrom(_from,_to,_amount);
+    }
+
+
+    function freezeTokens(address _userAddress, uint256 _amount) external notBlacklisted(msg.sender) notBlacklisted(_userAddress) onlyAdmin {
         uint256 balance = balanceOf(_userAddress);
         require(balance >= frozenTokens[_userAddress] + _amount, "Amount exceeds available balance");
         frozenTokens[_userAddress] = frozenTokens[_userAddress] + (_amount);
         emit TokensFrozen(_userAddress, _amount);
         }
 
-    function unfreezeTokens(address _userAddress, uint256 _amount) external  onlyAdmin {
+    function unfreezeTokens(address _userAddress, uint256 _amount) external notBlacklisted(msg.sender) notBlacklisted(_userAddress) onlyAdmin {
         require(frozenTokens[_userAddress] >= _amount, "Amount should be less than or equal to frozen tokens");
         frozenTokens[_userAddress] = frozenTokens[_userAddress] - (_amount);
         emit TokensUnfrozen(_userAddress, _amount);
@@ -72,34 +90,34 @@ contract TokenContract is ERC20,Ownable{
 
 
 
-     function addBlackList(address _userAddress,bool _freeze) external onlyAdmin {
-         blankList[_userAddress] = _freeze;
+     function addBlackList(address _userAddress,bool _freeze) external notBlacklisted(msg.sender) notBlacklisted(_userAddress) onlyAdmin {
+         _blacklisted[_userAddress] = _freeze;
         emit AddressFrozen(_userAddress, _freeze, msg.sender);
        }
 
   
 
-    function addBlackListBatch(address[] memory _blankList) external onlyAdmin{
+    function addBlackListBatch(address[] memory _blankList) external notBlacklisted(msg.sender)  onlyAdmin{
 
         for(uint i = 0; i<_blankList.length;i++){
-             require(!blankList[_blankList[i]], "address already blacklisted");
+             require(!_blacklisted[_blankList[i]], "address already blacklisted");
 
-            blankList[_blankList[i]] = true;
+            _blacklisted[_blankList[i]] = true;
 
         }
-        emit blankListed(_blankList);
+        emit blackListed(_blankList);
         
 
 
 
     }
 
-    function removeBlackkListBatch(address[] memory _removeBlankList) external onlyAdmin{
+    function removeBlackkListBatch(address[] memory _removeBlankList) external notBlacklisted(msg.sender) onlyAdmin{
 
         for(uint i = 0; i<_removeBlankList.length;i++){
-            require(blankList[_removeBlankList[i]], "address not blacklisted");
+            require(_blacklisted[_removeBlankList[i]], "address not blacklisted");
 
-            blankList[_removeBlankList[i]] = false;
+            _blacklisted[_removeBlankList[i]] = false;
 
         }
         emit removeBlankListed(_removeBlankList);
@@ -109,7 +127,7 @@ contract TokenContract is ERC20,Ownable{
     }
 
      function isBlacklisted(address account) external view returns (bool) {
-        return blankList[account];
+        return _blacklisted[account];
     }
 
     function adminRole(address _adminAddress) external onlyOwner {
