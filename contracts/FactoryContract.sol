d774593261bb70adb1a263cb0bc234668e9b4196
Hemanth Bhushan <hemanth.bhushan@antiersolutions.com>
2023-02-22 11:22:48 +0530

    Biconomy Added

--- contracts/FactoryContract.sol
+++ contracts/FactoryContract.sol
@@ -3,16 +3,15 @@ pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
+import "./BasicMetaTransaction.sol";
 
- contract FactoryContract is Ownable{
-    
-    address public implementation; 
-   
+contract FactoryContract is BasicMetaTransaction, Ownable {
+    address public implementation;
+
     address public adminAddress;
 
-
-
-    struct TokenDetails{
+    struct TokenDetails {
         string name;
         string symbol;
         uint8 decimals;
@@ -20,8 +19,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
     }
 
     address[] public storeAddress;
-    mapping(address=>TokenDetails) public registerToken;
-    mapping(address=>bool) public registered;
+    mapping(address => TokenDetails) public registerToken;
+    mapping(address => bool) public registered;
 
     event tokenRegistered(
         address indexed tokenAddress,
@@ -34,89 +33,88 @@ import "@openzeppelin/contracts/access/Ownable.sol";
         string indexed symbol
     );
 
-
-      modifier onlyAdmin() {
-        require(adminAddress == msg.sender, "onlyAdmin");
+    modifier onlyAdmin() {
+        require(adminAddress == msgSender(), "onlyAdmin");
         _;
     }
 
-    
-    constructor(address _implementation){
+    constructor(address _implementation)
+        
+    {
         implementation = _implementation;
     }
- 
 
     function adminRole(address _adminAddress) external onlyOwner {
         adminAddress = _adminAddress;
     }
 
-
-
-    function CreateERC20Token(
+    function CreateToken(
         string calldata _name,
         string calldata _symbol,
         uint8 _decimals,
         uint256 _initialSupply
     ) external onlyAdmin returns (address tokenAddress) {
+        tokenAddress = Clones.clone(implementation);
+        registerTokens(
+            TokenDetails({
+                name: _name,
+                symbol: _symbol,
+                decimals: _decimals,
+                initialSupply: _initialSupply
+            }),
+            address(tokenAddress)
+        );
 
-         tokenAddress = Clones.clone(implementation);
-        registerTokens(TokenDetails({name:_name,symbol:_symbol,decimals:_decimals,initialSupply: _initialSupply}),address(tokenAddress));
-
-        
         return tokenAddress;
     }
 
-
-     
-    function registerTokens(TokenDetails memory tokenDetails,address _tokenAddress) public onlyAdmin  {
-
-        require(_tokenAddress!= address(0) ,"invalid Token address");
-        require(!registered[_tokenAddress],"token address is already registered");
+    function registerTokens(
+        TokenDetails memory tokenDetails,
+        address _tokenAddress
+    ) public onlyAdmin {
+        require(_tokenAddress != address(0), "invalid Token address");
+        require(
+            !registered[_tokenAddress],
+            "token address is already registered"
+        );
 
         registerToken[_tokenAddress] = TokenDetails({
-            name:tokenDetails.name,
-            symbol:tokenDetails.symbol,
-            decimals:tokenDetails.decimals,
-            initialSupply:tokenDetails.initialSupply
+            name: tokenDetails.name,
+            symbol: tokenDetails.symbol,
+            decimals: tokenDetails.decimals,
+            initialSupply: tokenDetails.initialSupply
         });
-        registered[_tokenAddress] = true; 
+        registered[_tokenAddress] = true;
         storeAddress.push(_tokenAddress);
 
-
-        emit tokenRegistered(_tokenAddress,tokenDetails);
+        emit tokenRegistered(_tokenAddress, tokenDetails);
     }
 
+    function unregisterTokens(address _tokenAddress) external onlyAdmin {
+        require(_tokenAddress != address(0), "invalid Token address");
+        require(registered[_tokenAddress], "token address is not registered");
 
+        TokenDetails memory details = registerToken[_tokenAddress];
 
-    function unregisterTokens(address _tokenAddress) external onlyAdmin  {
-
-        require(_tokenAddress!= address(0) ,"invalid Token address");
-        require(registered[_tokenAddress],"token address is not registered");
-        
-
-         TokenDetails memory details = registerToken[_tokenAddress];
-
-
-         for(uint i=0;i<storeAddress.length;i++){
-
-         if(storeAddress[i]== _tokenAddress){
-                storeAddress[i] = storeAddress[storeAddress.length-1];
+        for (uint256 i = 0; i < storeAddress.length; i++) {
+            if (storeAddress[i] == _tokenAddress) {
+                storeAddress[i] = storeAddress[storeAddress.length - 1];
                 storeAddress.pop();
-                 }
-         }
+            }
+        }
 
-         
+        delete registerToken[_tokenAddress];
 
-         delete registerToken[_tokenAddress]; 
-
-
-        emit tokenUnregistered(_tokenAddress,details.name,details.symbol);
-
+        emit tokenUnregistered(_tokenAddress, details.name, details.symbol);
     }
 
-     function transferOwnershipOnTokenContract(address _newOwner) external  onlyOwner {
+    function transferOwnershipOnTokenContract(address _newOwner)
+        external
+        onlyOwner
+    {
         transferOwnership(_newOwner);
     }
 
+    
 
- }
+}
